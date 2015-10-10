a4bd9380272fa5912a0041b20e82fa3ba7d5d665
Jeam Alves <jeanmalves@gmail.com>
2015-10-10 17:52:22 -0300

    Criação da pagina sobre jeam

---
+++ sobre.php
@@ -0,0 +1,31 @@
+<?php include_once "header.php"; ?>
+<section id="content">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h4>Sobre Jeam Alves</h4>
+				<br>
+				<div class="col-lg-2">
+					<img src="img/jeam.jpg" alt="" class="align-left">
+				</div>
+				<p>
+					<strong>Jeam Alves</strong> é webdesigner desde 2002 e desenvolvedor web desde 2005. É acadêmico do curso de Sistemas de Informação pela UTFPR - Universidade Tecnológica Federal do Paraná, fundador do grupo de entusiastas em programação Coders UTFPR e participa da comunidade PHP PR.
+				</p>
+				<br>
+				<p>
+					Já trabalhou em agências de comunicação, software house, empresas de recrutamento e seleção e startup. 
+				</p>
+				<br>
+				<p>
+					Tem experiência com diversas linguagens de programação, também possui conhecimentos em consumo de APi's restfull e SOAP, desenvolvimento com HTML e CSS, JQuery, javascript, Ajax, frameworks para desenvolvimento web, além de design responsivo.
+				</p>
+				<br>
+				<p>
+					Atualmente trabalha com desenvolvimento web Front End e Back end, wordpress e criação de layouts para sites e sistemas.
+				</p>
+				<br>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include_once "footer.php"; ?>
